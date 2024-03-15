216488749c03e3af0b7a5a5e288a118e040850c4
Sai Vinay Kumar Banda <99705067+Saivinay63@users.noreply.github.com>
2024-03-15 14:33:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
     // typing text animation script
     var typed = new Typed(".typing", {
         strings: ["Adventures", "Vacations", "Experiences","Destinations"],
-        typeSpeed: 100,
+        typeSpeed: 300,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
         strings: ["Adventures", "Vacations", "Experiences","Destinations"],
-        typeSpeed: 100,
+        typeSpeed: 300,
         backSpeed: 60,
         loop: true
     });
